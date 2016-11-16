81c557037504dd6d5755063d08d18803d3f38083
Vistor25 <victorvoronko@gmail.com>
2016-11-16 12:11:21 +0300

    add

---
+++ Task1/FibonacciGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class FibonacciGenerator
+    {
+        public static IEnumerable<int> GenerateFibonacci(int n)
+        {
+            int F0 = 0;
+            int F1 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int F = F0 + F1;
+                F0 = F1;
+                F1 = F;
+                yield return F;
+            }
+        }
+    }
+}
